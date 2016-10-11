64649104dbd3338f0eaee12427964285b67136a5
f41c0r <f41c0r@hack.ink>
2016-10-10 21:46:53 -0700

    Added script to compute assets minus liabilities

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 This is a collection of various python scripts to add to gnucash functionality that I found was missing in the main program.
 
 * calculate-credit-card-usage-permonth.py: Shows you a running total of sums for spent / paid off amounts on a credit card.
+* assets-minus-liabilities.py: Calculates how much money you have by performing a sum of your assets minus your liabilities
 
 ## License:
 
---
+++ assets-minus-liabilities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+"""Calculates how much money you have
+
+assets-minus-liabilities.py calculates how much money you have on hand by
+subtracting the amount of money in your assets accounts... by the amount
+of money in your liabilities accounts.
+"""
+
+import gnucash
+from datetime import date
+from collections import OrderedDict
+
+__author__ = "f41c0r"
+__copyright__ = "Copyright 2016, f41c0r"
+__credits__ = ["f41c0r"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "f41c0r"
+__email__ = "f41c0r@hack.ink"
+__status__ = "Production"
+
+
+if __name__ == "__main__":
+
+    session = gnucash.Session("/path/to/accounts.gnucash")
+    book = session.get_book()
+    rootAcc = book.get_root_account()
+    assetsAcc = rootAcc.lookup_by_name("Assets")
+    liabilitiesAcc = rootAcc.lookup_by_name("Liabilities")
+
+    sumAssets = assetsAcc.GetBalance().to_double()
+    print("Assets (Root Account): ".ljust(32) + str(sumAssets).rjust(12))
+    assetsList = assetsAcc.get_descendants()
+    for descendant in assetsList:
+        print((descendant.name + ": ").ljust(32) + str(descendant.GetBalance()).rjust(12))
+        sumAssets = descendant.GetBalance().to_double()
+    
+    print
+
+    sumLiabilities = liabilitiesAcc.GetBalance().to_double()
+    print("Liabilities (Root Account): ".ljust(32) + str(sumLiabilities).rjust(12))
+    liabilitiesList = liabilitiesAcc.get_descendants()
+    for descendant in liabilitiesList:
+        print((descendant.name + ": ").ljust(32) + str(descendant.GetBalance()).rjust(12))
+        sumLiabilities += descendant.GetBalance().to_double()
+
+    print
+    print
+    print("Sum Total Assets:".ljust(32) + str(sumAssets).rjust(12))
+    print("Sum Total Liabilities:".ljust(32) + str(abs(sumLiabilities)).rjust(12))
+    print("Net Money on Hand".ljust(32) + str(sumAssets - abs(sumLiabilities)).rjust(12))
+
+    session.end()
